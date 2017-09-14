9afea9bcbc6aaca8087821524d3b218e0b9be183
udaypandu <udaychinnu20@gmail.com>
2017-09-14 11:13:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/nevexa/muchatlu/MainActivity.java
+++ app/src/main/java/com/nevexa/muchatlu/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
+                finish();
             }
         });
 
